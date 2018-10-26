372b03d5625846b1476920b716fbc09d8d8e96f4
yinqing <yinqing1989@126.com>
2018-10-26 14:31:49 +0800

    初始

---
+++ excelProcess/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ExcelOperation;
+
+namespace excelProcess
+{
+    public partial class Form1 : Form
+    {
+        private string ExcelConnStr;
+        private string ExcelStr;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string fName;
+            openFileDialog1.InitialDirectory = "E:\\考勤";//注意这里写路径时要用c:\\而不是c:\
+            openFileDialog1.Filter = "Excel2003文件|*.xls|Excel文件|*.xlsx";
+            openFileDialog1.RestoreDirectory = false;
+            openFileDialog1.FilterIndex = 1;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                fName = openFileDialog1.FileName;//全路径
+                getDataTable(fName);
+
+
+            }
+        }
+
+        private DataTable getDataTable(string path)
+        {
+            ExcelConnStr = "Provider=Microsoft.ACE.OLEDB.12.0;" + "Data Source=" + path + ";" + "Extended Properties='Excel 8.0;HDR=Yes;IMEX=1';";
+            ExcelStr = " SELECT * FROM [Sheet 1$] ";
+            DataTable dt = ExcelOperation.Operation.GetExcelSearchTable(ExcelConnStr, ExcelStr);
+            return dt;
+        }
+    }
+}
---
+++ excelProcess/Operation.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.OleDb;
+using System.Data;
+
+namespace ExcelOperation
+{
+    public class Operation
+    {
+        private static string _ExcelPath;  //Excel表格地址
+        private static string _ConnStr;    //连接Excel表格字符串
+        /// <summary>
+        /// Excel表格地址
+        /// </summary>
+        public static string ExcelPath
+        {
+            get { return _ExcelPath; }
+            set { _ExcelPath = value; }
+        }
+        /// <summary>
+        /// 连接Excel表格字符串
+        /// </summary>
+        public static string ConnStr
+        {
+            get { return _ConnStr; }
+            set { _ConnStr = value; }
+        }
+        /// <summary>
+        /// 获取查询Excel表格结果
+        /// </summary>
+        /// <param name="strConn">连接Excel表字符串</param>
+        /// <param name="ExcelSearchStr">Excel表格查询语句。注：from [Sheet1$]";//这里Sheet1对应excel的工作表名称，"$"符号是必须的</param>
+        /// <returns>查询结果表</returns>
+        public static DataTable GetExcelSearchTable(string strConn, string ExcelSearchStr)
+        {
+            DataSet ds = null;
+            System.Data.DataTable dt = null;
+            OleDbConnection conn = new OleDbConnection(strConn);
+            conn.Open();
+            OleDbDataAdapter myCommand = null;
+            myCommand = new OleDbDataAdapter(ExcelSearchStr, strConn);
+            ds = new DataSet();
+            myCommand.Fill(ds, "table1");
+            dt = ds.Tables[0];
+            conn.Close();
+            return dt;
+        }
+    }
+}
